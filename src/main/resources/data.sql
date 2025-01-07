88d30a15bd068273b429b0df13e07541c24f5ae9
Gerdany Junior <Asa_lindo@hotmail.com>
2025-01-06 22:28:13 -0300

    feat: add initialization data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO
+    tipo_ocorrencia (id, descricao)
+VALUES
+    (1, 'Acidente de Trânsito'),
+    (2, 'Congestionamento'),
+    (3, 'Veículo Quebrado'),
+    (4, 'Obra na Via'),
+    (5, 'Condição Climática Adversa'),
+    (6, 'Evento Público'),
+    (7, 'Fiscalização de Trânsito'),
+    (8, 'Interdição de Via'),
+    (9, 'Semáforo Quebrado'),
+    (10, 'Manifestação');
