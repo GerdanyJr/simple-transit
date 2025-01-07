8770c67be330b93aed6fd93a4e93947e26f89536
Gerdany Junior <Asa_lindo@hotmail.com>
2025-01-07 01:55:09 -0300

    feat: create login method on service

--- src/main/java/com/github/gerdanyjr/simple_transit/service/impl/AuthServiceImpl.java
+++ src/main/java/com/github/gerdanyjr/simple_transit/service/impl/AuthServiceImpl.java
@@ -1,22 +1,33 @@
 package com.github.gerdanyjr.simple_transit.service.impl;
 
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.github.gerdanyjr.simple_transit.model.dto.req.LoginReq;
 import com.github.gerdanyjr.simple_transit.model.dto.req.RegisterUserReq;
+import com.github.gerdanyjr.simple_transit.model.dto.res.TokenRes;
 import com.github.gerdanyjr.simple_transit.model.entity.User;
 import com.github.gerdanyjr.simple_transit.model.exception.impl.ConflictException;
 import com.github.gerdanyjr.simple_transit.repository.UserRepository;
 import com.github.gerdanyjr.simple_transit.service.AuthService;
+import com.github.gerdanyjr.simple_transit.service.TokenService;
 import com.github.gerdanyjr.simple_transit.util.Mapper;
 
 @Service
 public class AuthServiceImpl implements AuthService {
 
+    private final AuthenticationManager authenticationManager;
+    private final TokenService tokenService;
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public AuthServiceImpl(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+    public AuthServiceImpl(AuthenticationManager authenticationManager, TokenService tokenService,
+            UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.authenticationManager = authenticationManager;
+        this.tokenService = tokenService;
         this.userRepository = userRepository;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
@@ -32,6 +43,22 @@ public class AuthServiceImpl implements AuthService {
         User createdUser = Mapper.fromRegisterReqToUser(req, bCryptPasswordEncoder);
 
         return userRepository.save(createdUser);
+    }
+
+    @Override
+    public TokenRes login(LoginReq req) {
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                req.login(),
+                req.password());
+
+        Authentication authenticate = authenticationManager.authenticate(authToken);
+
+        User user = (User) authenticate.getPrincipal();
+
+        String accessToken = tokenService.generateAccessToken(user);
+        String refreshToken = tokenService.generateRefreshToken(user);
+
+        return new TokenRes(accessToken, refreshToken);
     }
 
 }
