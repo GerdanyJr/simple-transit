a40f0a60dae69e55096539bf273977b17aebcc6a
Gerdany Junior <Asa_lindo@hotmail.com>
2025-01-06 22:25:07 -0300

    first commit

---
+++ src/main/java/com/gihub/gerdanyjr/simple_transit/SimpleTransitApplication.java
@@ -0,0 +1,13 @@
+package com.gihub.gerdanyjr.simple_transit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleTransitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleTransitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gihub/gerdanyjr/simple_transit/SimpleTransitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gihub.gerdanyjr.simple_transit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleTransitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
