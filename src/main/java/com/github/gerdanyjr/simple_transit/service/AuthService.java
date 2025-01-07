c7ad0a2ff57c4325a2dd0cb4e5d25b6508fc0a9d
Gerdany Junior <Asa_lindo@hotmail.com>
2025-01-07 13:35:17 -0300

    feat: implement updateUser on service

--- src/main/java/com/github/gerdanyjr/simple_transit/service/AuthService.java
+++ src/main/java/com/github/gerdanyjr/simple_transit/service/AuthService.java
@@ -1,7 +1,10 @@
 package com.github.gerdanyjr.simple_transit.service;
 
+import java.security.Principal;
+
 import com.github.gerdanyjr.simple_transit.model.dto.req.LoginReq;
 import com.github.gerdanyjr.simple_transit.model.dto.req.RegisterUserReq;
+import com.github.gerdanyjr.simple_transit.model.dto.req.UpdateUserReq;
 import com.github.gerdanyjr.simple_transit.model.dto.res.TokenRes;
 import com.github.gerdanyjr.simple_transit.model.entity.User;
 
@@ -12,4 +15,6 @@ public interface AuthService {
     TokenRes login(LoginReq req);
 
     TokenRes refreshToken(String refreshToken);
+
+    User updateUser(UpdateUserReq req, Principal principal);
 }
