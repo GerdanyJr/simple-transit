50c36fa838e08fa19b707ebf5994a9a2b1e1f764
Gerdany Junior <Asa_lindo@hotmail.com>
2025-01-07 00:03:27 -0300

    feat: create exception handler for validation errors

--- src/main/java/com/github/gerdanyjr/simple_transit/config/ExceptionHandlerConfig.java
+++ src/main/java/com/github/gerdanyjr/simple_transit/config/ExceptionHandlerConfig.java
@@ -1,10 +1,17 @@
 package com.github.gerdanyjr.simple_transit.config;
 
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.github.gerdanyjr.simple_transit.model.dto.res.ErrorResponse;
+import com.github.gerdanyjr.simple_transit.model.dto.res.ValidationErrorResponse;
 import com.github.gerdanyjr.simple_transit.model.exception.BaseException;
 
 @RestControllerAdvice
@@ -18,5 +25,23 @@ public class ExceptionHandlerConfig {
                         e.getTimestamp(),
                         e.getHttpStatus(),
                         e.getCode()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationErrorResponse> methodArgumentNotValidExceptionHandler(
+            MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+
+        e.getFieldErrors()
+                .forEach(error -> errors
+                        .put(error.getField(),
+                                error.getDefaultMessage()));
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ValidationErrorResponse(errors,
+                        Instant.now(),
+                        HttpStatus.BAD_REQUEST,
+                        HttpStatus.BAD_REQUEST.value()));
     }
 }
