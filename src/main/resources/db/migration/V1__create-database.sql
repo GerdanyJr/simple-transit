86b3bb3d65b8128f023e703add227c83f56439fa
Gerdany Junior <Asa_lindo@hotmail.com>
2025-01-06 22:28:03 -0300

    chore: create database migration

---
+++ src/main/resources/db/migration/V1__create-database.sql
@@ -0,0 +1,35 @@
+CREATE TABLE tipo_ocorrencia(
+    id INTEGER PRIMARY KEY NOT NULL UNIQUE,
+    descricao VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE usuario(
+    id INTEGER PRIMARY KEY NOT NULL UNIQUE,
+    nome VARCHAR(50) NOT NULL,
+    login VARCHAR(20) NOT NULL UNIQUE,
+    senha VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE ocorrencia(
+    id INTEGER PRIMARY KEY NOT NULL UNIQUE,
+    resumo VARCHAR(100) NOT NULL,
+    descricao VARCHAR(255) NOT NULL,
+    data_hora TIMESTAMP NOT NULL,
+    endereco VARCHAR(100) NOT NULL,
+    latitude NUMERIC,
+    longitude NUMERIC,
+    tipo_ocorrencia_id INTEGER NOT NULL,
+    usuario_id INTEGER NOT NULL,
+    FOREIGN KEY (tipo_ocorrencia_id) REFERENCES tipo_ocorrencia(id),
+    FOREIGN KEY (usuario_id) REFERENCES usuario(id)
+);
+
+CREATE TABLE comentario(
+    id INTEGER PRIMARY KEY NOT NULL UNIQUE,
+    comentario VARCHAR(140) NOT NULL,
+    data_hora TIMESTAMP NOT NULL,
+    ocorrencia_id INTEGER NOT NULL,
+    usuario_id INTEGER NOT NULL,
+    FOREIGN KEY (ocorrencia_id) REFERENCES ocorrencia(id),
+    FOREIGN KEY (usuario_id) REFERENCES usuario(id)
+);
