2c9f97796164eee4b79f564b4ce25a247fd1a573
Gerdany Junior <Asa_lindo@hotmail.com>
2025-01-07 20:04:22 -0300

    test: create test cases for createReport

---
+++ src/test/java/com/github/gerdanyjr/simple_transit/service/ReportServiceTest.java
@@ -0,0 +1,152 @@
+package com.github.gerdanyjr.simple_transit.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.security.Principal;
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.github.gerdanyjr.simple_transit.model.dto.req.CreateReportReq;
+import com.github.gerdanyjr.simple_transit.model.entity.Report;
+import com.github.gerdanyjr.simple_transit.model.entity.ReportType;
+import com.github.gerdanyjr.simple_transit.model.entity.User;
+import com.github.gerdanyjr.simple_transit.model.exception.impl.NotFoundException;
+import com.github.gerdanyjr.simple_transit.repository.ReportRepository;
+import com.github.gerdanyjr.simple_transit.repository.ReportTypeRepository;
+import com.github.gerdanyjr.simple_transit.repository.UserRepository;
+import com.github.gerdanyjr.simple_transit.service.impl.ReportServiceImpl;
+
+@ExtendWith(MockitoExtension.class)
+public class ReportServiceTest {
+
+    @Mock
+    private ReportRepository reportRepository;
+
+    @Mock
+    private ReportTypeRepository reportTypeRepository;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private ReportServiceImpl reportService;
+
+    private Report mockReport;
+
+    private ReportType mockReportType;
+
+    private User mockUser;
+
+    private CreateReportReq req;
+
+    private Principal principal;
+
+    @BeforeEach
+    void setup() {
+        principal = mock(Principal.class);
+
+        mockReportType = new ReportType(1, "Description");
+
+        mockUser = new User(1,
+                "Name",
+                "Login",
+                "Password",
+                List.of());
+
+        mockReport = new Report(null,
+                "Summary",
+                "Description",
+                Instant.now(),
+                "Address",
+                -12.9714,
+                -15.9714,
+                null,
+                mockUser,
+                mockReportType);
+
+        req = new CreateReportReq("Summary",
+                "Description",
+                Instant.now(),
+                "Address",
+                -12.9714,
+                -15.9714,
+                1);
+    }
+
+    @Test
+    @DisplayName("should return created report when a valid report is passed")
+    void givenValidReport_whenCreate_thenReturnCreatedReport() {
+        when(principal.getName())
+                .thenReturn(mockUser.getLogin());
+
+        when(reportTypeRepository.findById(anyInt()))
+                .thenReturn(Optional.of(mockReportType));
+
+        when(userRepository.findByLogin(anyString()))
+                .thenReturn(Optional.of(mockUser));
+
+        when(reportRepository.save(any(Report.class)))
+                .thenReturn(mockReport);
+
+        Report result = reportService.create(req, principal);
+
+        assertEquals(mockReport, result);
+        verify(reportTypeRepository).findById(1);
+        verify(userRepository).findByLogin("Login");
+        verify(reportRepository).save(mockReport);
+    }
+
+    @Test
+    @DisplayName("should throw a exception when a inexistent reportTypeId is passed")
+    void givenInvalidReportTypeId_whenCreate_thenThrowNotFoundException() {
+        when(reportTypeRepository.findById(anyInt()))
+                .thenReturn(Optional.empty());
+
+        NotFoundException e = assertThrows(NotFoundException.class,
+                () -> {
+                    reportService.create(req, principal);
+                });
+
+        assertEquals(e.getMessage(), "ReportType não encontrado com id: 1");
+        verify(reportRepository, never()).save(any(Report.class));
+    }
+
+    @Test
+    @DisplayName("should throw a exception when a inexistent principal is provided")
+    void givenInvalidPrincipal_whenCreate_thenThrowNotFoundException() {
+        when(reportTypeRepository.findById(anyInt()))
+                .thenReturn(Optional.of(mockReportType));
+
+        when(userRepository.findByLogin(anyString()))
+                .thenReturn(Optional.empty());
+
+        when(principal.getName())
+                .thenReturn(mockUser.getLogin());
+
+        NotFoundException e = assertThrows(NotFoundException.class,
+                () -> {
+                    reportService.create(req, principal);
+                });
+
+        assertEquals(e.getMessage(), "Usuário não encontrado com login: " + mockUser.getLogin());
+        verify(reportRepository, never()).save(any(Report.class));
+    }
+
+}
