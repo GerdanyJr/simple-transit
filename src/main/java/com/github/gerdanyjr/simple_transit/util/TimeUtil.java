9d98fc9967c62d73f828ff5be8de486a6f914d31
Gerdany Junior <Asa_lindo@hotmail.com>
2025-01-09 01:59:55 -0300

    feat: add @UtilityCLass on util classes to prevent instantiation

--- src/main/java/com/github/gerdanyjr/simple_transit/util/TimeUtil.java
+++ src/main/java/com/github/gerdanyjr/simple_transit/util/TimeUtil.java
@@ -3,6 +3,9 @@ package com.github.gerdanyjr.simple_transit.util;
 import java.time.Duration;
 import java.time.LocalDateTime;
 
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
 public class TimeUtil {
 
     public static Boolean isBeforeDaysAgo(LocalDateTime date, Integer daysBefore) {
