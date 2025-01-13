2c23e3794efc6a8f0e44b7c01bc669aa838a68a4
Gerdany Junior <Asa_lindo@hotmail.com>
2025-01-12 23:22:05 -0300

    reafctor: change timestamp format

--- src/main/java/com/github/gerdanyjr/simple_transit/model/dto/req/CreateReportReq.java
+++ src/main/java/com/github/gerdanyjr/simple_transit/model/dto/req/CreateReportReq.java
@@ -2,16 +2,18 @@ package com.github.gerdanyjr.simple_transit.model.dto.req;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record CreateReportReq(
-        @NotBlank String summary,
-        @NotBlank String description,
-        @NotNull LocalDateTime timestamp,
-        @NotBlank String address,
-        Double latitude,
-        Double longitude,
-        @NotNull Integer reportTypeId) {
+                @NotBlank String summary,
+                @NotBlank String description,
+                @NotNull @JsonFormat(pattern = "dd/MM/yyyy, HH:mm:ss") LocalDateTime timestamp,
+                @NotBlank String address,
+                Double latitude,
+                Double longitude,
+                @NotNull Integer reportTypeId) {
 
 }
